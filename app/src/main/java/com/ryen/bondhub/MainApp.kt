902795e3e33e5fceff80985578a64d5958de3485
sarfrazryenpsd <mdsarfraz.ilanos1915@gmail.com>
2025-01-21 23:26:33 +0530

    SignInScreen & SignUpScreen UI completed

--- app/src/main/java/com/ryen/bondhub/MainApp.kt
+++ app/src/main/java/com/ryen/bondhub/MainApp.kt
@@ -5,13 +5,23 @@ import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.ryen.bondhub.presentation.screens.auth.SignInScreen
+import com.ryen.bondhub.presentation.screens.auth.SignUpScreen
 
 @Composable
 fun MainApp() {
     Surface(modifier = Modifier.fillMaxSize()){
         val navController = rememberNavController()
 
-        NavHost(navController = navController, startDestination = "") { }
+        NavHost(navController = navController, startDestination = "signIn") {
+            composable("signIn"){
+                SignInScreen(navController = navController)
+            }
+            composable("signUp"){
+                SignUpScreen(navController = navController)
+            }
+        }
     }
 }
--- app/src/main/java/com/ryen/bondhub/presentation/theme/Color.kt
+++ app/src/main/java/com/ryen/bondhub/presentation/theme/Color.kt
@@ -9,3 +9,8 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val Surface = Color(0xFFF8F8F8)
+val Primary = Color(0xFFFC6D55)
+val Secondary = Color(0xFF524A4C)
+val Tertiary = Color(0xFF606060)
