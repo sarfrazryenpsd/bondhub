12a03cadaed1cf687f90d45d63651d33d4bf82bc
sarfrazryenpsd <mdsarfraz.ilanos1915@gmail.com>
2025-01-25 21:02:16 +0530

    Added and implemented auth repo and app module(DI). Also added auth usecases

---
+++ app/src/main/java/com/ryen/bondhub/domain/useCases/SignUpUseCase.kt
@@ -0,0 +1,13 @@
+package com.ryen.bondhub.domain.useCases
+
+import com.ryen.bondhub.domain.model.User
+import com.ryen.bondhub.domain.repository.AuthRepository
+import javax.inject.Inject
+
+class SignUpUseCase @Inject constructor(
+    private val repository: AuthRepository
+) {
+    suspend operator fun invoke(email: String, password: String, displayName: String): Result<User> {
+        return repository.signUp(email, password, displayName)
+    }
+}
