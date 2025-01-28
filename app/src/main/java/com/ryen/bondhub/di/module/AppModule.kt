51fc46400931a296f8fee8af6dbfd5c4c35df9a0
sarfrazryenpsd <mdsarfraz.ilanos1915@gmail.com>
2025-01-29 00:24:38 +0530

    Added feature: GetAuthState to determine if user is signed in and navigate to directly chat screen.

--- app/src/main/java/com/ryen/bondhub/di/module/AppModule.kt
+++ app/src/main/java/com/ryen/bondhub/di/module/AppModule.kt
@@ -3,6 +3,7 @@ package com.ryen.bondhub.di.module
 import com.google.firebase.auth.FirebaseAuth
 import com.ryen.bondhub.data.repository.AuthRepositoryImpl
 import com.ryen.bondhub.domain.repository.AuthRepository
+import com.ryen.bondhub.domain.useCases.GetAuthStateUseCase
 import com.ryen.bondhub.domain.useCases.SignInUseCase
 import com.ryen.bondhub.domain.useCases.SignUpUseCase
 import dagger.Module
@@ -30,5 +31,9 @@ object AppModule {
     @Provides
     @Singleton
     fun provideSignUpUseCase(repository: AuthRepository): SignUpUseCase = SignUpUseCase(repository)
+
+    @Provides
+    @Singleton
+    fun provideGetAuthStateUseCase(repository: AuthRepository): GetAuthStateUseCase = GetAuthStateUseCase(repository)
 
 }
--- app/src/main/java/com/ryen/bondhub/domain/repository/AuthRepository.kt
+++ app/src/main/java/com/ryen/bondhub/domain/repository/AuthRepository.kt
@@ -1,10 +1,12 @@
 package com.ryen.bondhub.domain.repository
 
 import com.ryen.bondhub.domain.model.User
+import kotlinx.coroutines.flow.Flow
 
 interface AuthRepository {
     suspend fun signIn(email: String, password: String): Result<User>
     suspend fun signUp(email: String, password: String, displayName: String): Result<User>
     suspend fun signOut(): Result<Unit>
     fun getCurrentUser(): User?
+    fun isUserAuthenticated(): Flow<Boolean>
 }
