0ec065be46b2a5dc219bc02f380ef7f722dfb28f
sarfrazryenpsd <mdsarfraz.ilanos1915@gmail.com>
2025-02-14 22:35:01 +0530

    Debugging userProfile creation

--- app/src/main/java/com/ryen/bondhub/presentation/state/AuthState.kt
+++ app/src/main/java/com/ryen/bondhub/presentation/state/AuthState.kt
@@ -1,10 +1,12 @@
 package com.ryen.bondhub.presentation.state
 
+import com.google.firebase.auth.FirebaseUser
 import com.ryen.bondhub.domain.model.UserAuth
 
 sealed class AuthState{
     data object Initial: AuthState()
     data object Loading: AuthState()
     data class Error(val message: String): AuthState()
-    data class Authenticated(val user: UserAuth): AuthState()
+    data class SignInSuccess(val user: UserAuth) : AuthState()
+    data class SignUpSuccess(val user: UserAuth) : AuthState()
 }
