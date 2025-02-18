b4308ece5a6574a13634a896915dc8d82ab37610
sarfrazryenpsd <mdsarfraz.ilanos1915@gmail.com>
2025-02-18 22:09:40 +0530

    Fixed flash screen as loading screen + provided auth repository instance by creating a hilt entrypoint and using CompositionLocalProvider

--- app/src/main/java/com/ryen/bondhub/presentation/screens/MainApp.kt
+++ app/src/main/java/com/ryen/bondhub/presentation/screens/MainApp.kt
@@ -9,70 +9,81 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.google.firebase.auth.FirebaseAuth
+import com.ryen.bondhub.di.module.LocalAuthRepository
+import com.ryen.bondhub.di.module.ProvideAuthRepository
 import com.ryen.bondhub.presentation.screens.auth.AuthScreen
 import com.ryen.bondhub.presentation.screens.chat.ChatScreen
 import com.ryen.bondhub.presentation.screens.chat.ProfileUpdateScreen
-import com.ryen.bondhub.presentation.theme.BondHubTheme
 
 
 @Composable
 fun MainApp(
     navController: NavHostController = rememberNavController()
 ) {
-    Surface(modifier = Modifier.fillMaxSize()) {
-        var startDestination by remember { mutableStateOf(Screen.AuthScreen.route) }
+    ProvideAuthRepository {
+        Surface(modifier = Modifier.fillMaxSize()) {
+            var startDestination by remember { mutableStateOf(Screen.LoadingScreen.route) }
+            val authRepository = LocalAuthRepository.current
 
-        LaunchedEffect(Unit) {
-            startDestination = if (FirebaseAuth.getInstance().currentUser != null) {
-                Screen.ChatScreen.route
-            } else {
-                Screen.AuthScreen.route
-            }
-        }
-
-        NavHost(
-            navController = navController,
-            startDestination = startDestination
-        ) {
-            composable(Screen.AuthScreen.route) {
-                AuthScreen(
-                    onNavigate = { route ->
-                        navController.navigate(route) {
-                            popUpTo(Screen.AuthScreen.route) { inclusive = true }
-                        }
+            LaunchedEffect(Unit) {
+                val currentUser = FirebaseAuth.getInstance().currentUser
+                if (currentUser != null) {
+                    startDestination = if (authRepository.isProfileSetupComplete()) {
+                        Screen.ChatScreen.route
+                    } else {
+                        Screen.UserProfileSetupScreen.route
                     }
-                )
+                }
             }
 
-            composable(Screen.UserProfileSetupScreen.route) {
-                ProfileUpdateScreen(
-                    onDone = {
-                        navController.navigate(Screen.ChatScreen.route) {
-                            popUpTo(Screen.UserProfileSetupScreen.route) { inclusive = true }
-                        }
-                    },
-                    onSkip = {
-                        navController.navigate(Screen.ChatScreen.route) {
-                            popUpTo(Screen.UserProfileSetupScreen.route) { inclusive = true }
-                        }
-                    }
-                )
-            }
 
-            composable(Screen.ChatScreen.route) {
-                ChatScreen(
-                    onNavigateToAuth = {
-                        navController.navigate(Screen.AuthScreen.route) {
-                            popUpTo(Screen.ChatScreen.route) { inclusive = false }
+
+            NavHost(
+                navController = navController,
+                startDestination = startDestination
+            ) {
+                composable(Screen.LoadingScreen.route) {
+                    LoadingScreen()
+                }
+                composable(Screen.AuthScreen.route) {
+                    AuthScreen(
+                        onNavigate = { route ->
+                            navController.navigate(route) {
+                                popUpTo(Screen.AuthScreen.route) { inclusive = true }
+                            }
                         }
-                    }
-                )
+                    )
+                }
+
+                composable(Screen.UserProfileSetupScreen.route) {
+                    ProfileUpdateScreen(
+                        onDone = {
+                            navController.navigate(Screen.ChatScreen.route) {
+                                popUpTo(Screen.UserProfileSetupScreen.route) { inclusive = true }
+                            }
+                        },
+                        onSkip = {
+                            navController.navigate(Screen.ChatScreen.route) {
+                                popUpTo(Screen.UserProfileSetupScreen.route) { inclusive = true }
+                            }
+                        }
+                    )
+                }
+
+                composable(Screen.ChatScreen.route) {
+                    ChatScreen(
+                        onNavigateToAuth = {
+                            navController.navigate(Screen.AuthScreen.route) {
+                                popUpTo(Screen.ChatScreen.route) { inclusive = false }
+                            }
+                        }
+                    )
+                }
             }
         }
     }
